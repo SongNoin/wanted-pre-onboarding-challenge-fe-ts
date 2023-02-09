a98dd45fcb8bf4f49c6a8abe8348ad542886714d
SongNoin <as5427072@gmail.com>
2023-02-09 15:30:36 +0900

    FEAT todo 함수 수정

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
  * @property {string} content - 내용
  * @property {boolean} isDone - 완료여부
  * @property {string} category - 카테고리
- * @property {string[]?} tags - 적용할 태그 배열
+ * @property {string[]} tags - 적용할 태그 배열
  */
 
 /**
