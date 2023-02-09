18d20c84958f620f5adc0ad80a10f630253eae85
SongNoin <as5427072@gmail.com>
2023-02-09 15:26:12 +0900

    BUILD create 타입 선언

--- src/index.js
+++ src/index.js
@@ -7,3 +7,13 @@
  * @property {string} category - 카테고리
  * @property {string[]?} tags - 적용할 태그 배열
  */
+
+/**
+ * 할 일을 추가한다.
+ * 내용없이 추가 할 수 없다.
+ * @param {object} newTodo
+ * @param {string} newTodo.text - 할 일의 내용
+ * @param {string} newTodo.category - 할 일의 카테고리
+ * @param {string[]} [newTodo.tag] - 할 일의 태그 배열
+ */
+function createTodo({ text, category, tags }) {}
