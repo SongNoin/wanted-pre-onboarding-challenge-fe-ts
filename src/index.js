0022fc604308342ab66ee07ba94e1a27c761014c
SongNoin <as5427072@gmail.com>
2023-02-09 16:22:54 +0900

    FIX 수정사항들 수정

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
  * @property {string} content - 내용
  * @property {boolean} isDone - 완료여부
  * @property {string} category - 카테고리
- * @property {string[]} tags - 적용할 태그 배열
+ * @property {string[]} [tags] - 적용할 tag 배열
  */
 
 /**
@@ -14,9 +14,9 @@
  * @param {object} newTodo
  * @param {string} newTodo.text - 할 일의 내용
  * @param {string} newTodo.category - 할 일의 카테고리
- * @param {string[]} [newTodo.tags] - 할 일의 태그 배열
+ * @param {string[]} [newTodo.tags] - 할 일의 tag 배열
  */
-function createTodo({ text, category, tags }) {}
+function createTodo(newTodo) {}
 
 /**
  * 모든 할 일을 조회할 수 있다.
@@ -34,23 +34,24 @@ function getTodoById(id) {}
 /**
  * ID를 제외한 모든 속성을 수정할 수 있다.
  * @param {object} todo
- * @param {string} todo.content - 변경할 내용
- * @param {boolean} todo.isDone - 완료여부 변경
- * @param {string} todo.category - 변경할 카테고리
+ * @param {string} [todo.content] - 변경할 내용
+ * @param {boolean} [todo.isDone] - 완료여부 변경
+ * @param {string} [todo.category] - 변경할 카테고리
  */
 function updateTodo(todo) {}
 
 /**
- * 특정 할 일의 특정 태그를 수정할 수 있다.
- * @param {Todo} todo
- * @param {string} oldTag - 기존 태그
- * @param {string} newTag - 변경할 태그
+ * 특정 할 일의 특정 tag를 수정할 수 있다.
+ * id 기반으로 특정 할 일의 tag에 해당하는 값을 newTag로 대체한다.
+ * @param {number} id - 특정 할 일의 ID
+ * @param {string} tag - 기존 tag
+ * @param {string} newTag - 변경할 tag
  */
-function updateTag(todo, oldTag, newTag) {}
+function updateTag(id, tag, newTag) {}
 
 /**
  * ID를 기반으로 특정 할 일을 삭제할 수 있다.
- * @param {string} id - 삭제할 할 일의 ID
+ * @param {number} id - 삭제할 할 일의 ID
  */
 function deleteTodoById(id) {}
 
@@ -60,14 +61,16 @@ function deleteTodoById(id) {}
 function deleteAllTodo() {}
 
 /**
- * 특정 할 일의 특정 태그를 삭제할 수 있다.
- * @param {Todo} todo
- * @param {string} tag
+ * 특정 할 일의 특정 tag를 삭제할 수 있다.
+ * id 기반으로 특정 할 일의 tag에 해당하는 값을 삭제한다.
+ * @param {number} id - 특정 할 일의 ID
+ * @param {string} tag- 삭제할 tag
  */
-function deleteTag(todo, tag) {}
+function deleteTag(id, tag) {}
 
 /**
- * 특정 할 일의 특정 태그를 삭제할 수 있다.
- * @param {Todo} todo
+ * 특정 할 일의 모든 tag를 삭제할 수 있다.
+ * id 기반으로 특정 할 일의 모든 tag 를 삭제한다.
+ * @param {number} id - 특정 할 일의 ID
  */
-function deleteAllTag(todo) {}
+function deleteAllTag(id) {}
